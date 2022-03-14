148813fc16c9b853775a17fa42431b263c97f7e3
Angela Rosas Venegas <73150391+arosasvenegas@users.noreply.github.com>
2022-03-14 19:05:50 -0300

    vista muro, categorias, vista muro tablet

--- src/lib/muro.js
+++ src/lib/muro.js
@@ -1,5 +1,6 @@
 import { getAuth,signOut} from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
 import { app } from "../Firebase.js";
+import { food } from "./alimento.js";
 
 export function muroPage() {
 
@@ -9,20 +10,37 @@ export function muroPage() {
     
     
     const muroView = `<div class="containerMuro" id="containerMuro">
+     <header class="encabezadoMuro" id="encabezadoMuro">
+      <img src="/imagenes/logo-lucchi.png" id="logoMuro" class="logoMuro">
+     </header>
 
-     <h1>HELLO THERE<h1>
-      <p> general kenobi </p> 
-     
+     <section class="categoryHome" id="categoryHome">
+     <button class="categoryFood" id="btnfood">Alimentacion</button>
+     <button class="categoryclean" id="categoryclean">Higine</button>
+     <button class="categoryVet" id="categoryVet">Veterinario</button>
+     <button class="categoryGo" id="categoryGo">Paseo</button>
+     <button class="categoryPlay" id="categoryPlay">Juegos</button>
+     <button class="categoryAcc" id="categoryAcc">Accesorios</button>
+     </section>
+
+      
       <button class="logOut" id="btnLogOut">cerrar sesión</button>
+      
       </div>`;
   
      muroV.innerHTML=muroView
 
+
+    let btnComida = muroV.querySelector('#btnfood');
+    btnComida.addEventListener('click', () => {
+         food();
+      });
   
     let btnSalirV = muroV.querySelector('#btnLogOut');
     btnSalirV.addEventListener('click', () => {
       logOut();
     });
+
     
   
   return muroV;
@@ -34,7 +52,7 @@ export function muroPage() {
   // cerrar sesion
 function logOut() {
     signOut(auth).then(() => {
-      alert("Usted esta cerrando sesion");
+      alert("Usted esta cerrando sesion✌🏻");
       window.location.hash = '#/welcome';
     }).catch((error) => {
     
